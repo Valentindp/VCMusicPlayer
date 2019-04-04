f7a1d214b9c38d1d4b14b62d07e031ad3ad29d78
valentinchor <zeratul89>
2019-04-04 14:23:42 +0300

    Initial commit

---
+++ app/src/main/java/valentyn/vcmusicplayer/data/dataloaders/AlbumLoader.kt
@@ -0,0 +1,62 @@
+package com.example.valentin.valentinmusicplayer.data.dataloaders
+
+import android.content.Context
+import android.database.Cursor
+import android.provider.MediaStore
+import com.example.valentin.valentinmusicplayer.models.Album
+import java.util.ArrayList
+
+class AlbumLoader {
+
+    fun getAlbum(cursor: Cursor?): Album {
+        var album = Album()
+        if (cursor != null) {
+            if (cursor.moveToFirst())
+                album = Album(
+                    cursor.getLong(0),
+                    cursor.getString(1),
+                    cursor.getString(2),
+                    cursor.getLong(3),
+                    cursor.getInt(4),
+                    cursor.getInt(5)
+                )
+        }
+        cursor?.close()
+        return album
+    }
+
+    fun getAlbumsForCursor(cursor: Cursor?): MutableList<Album> {
+        val arrayList = ArrayList<Album>()
+        if (cursor != null && cursor.moveToFirst())
+            do {
+                arrayList.add(
+                    Album(
+                        cursor.getLong(0),
+                        cursor.getString(1),
+                        cursor.getString(2),
+                        cursor.getLong(3),
+                        cursor.getInt(4),
+                        cursor.getInt(5)
+                    )
+                )
+            } while (cursor.moveToNext())
+        cursor?.close()
+        return arrayList
+    }
+
+    fun getAllAlbums(context: Context) = getAlbumsForCursor(makeAlbumCursor(context, null, null))
+
+    fun getAlbum(context: Context, id: Long) = getAlbum(makeAlbumCursor(context, "_id=?", arrayOf(id.toString())))
+
+    fun getAlbums(context: Context, paramString: String) =
+        getAlbumsForCursor(makeAlbumCursor(context, "album LIKE ?", arrayOf("$paramString%")))
+
+    fun makeAlbumCursor(context: Context, selection: String?, paramArrayOfString: Array<String>?) =
+        context.contentResolver.query(
+            MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
+            arrayOf("_id", "album", "artist", "artist_id", "numsongs", "minyear"),
+            selection,
+            paramArrayOfString,
+            MediaStore.Audio.Albums.DEFAULT_SORT_ORDER
+        )
+}
---
+++ app/src/main/java/valentyn/vcmusicplayer/models/Album.kt
@@ -0,0 +1,12 @@
+package com.example.valentin.valentinmusicplayer.models
+
+data class Album (
+    val id: Long = -1,
+    val title: String = "",
+    val artistName: String = "",
+    val artistId: Long = -1,
+    val songCount: Int = -1,
+    val year: Int = -1)
+{
+
+}
