b1be53b65e84bcf11c79f4ec10813662fa081af7
valentinchor <zeratul89>
2019-04-15 17:42:51 +0300

    1.2

---
+++ app/src/main/java/valentyn/vcmusicplayer/albums/AlbumsFragment.kt
@@ -0,0 +1,7 @@
+package valentyn.vcmusicplayer.albums
+
+class AlbumsFragment {
+
+
+
+}
---
+++ app/src/main/java/valentyn/vcmusicplayer/utils/PlayerUtils.kt
@@ -0,0 +1,11 @@
+package valentyn.vcmusicplayer.utils
+
+import android.content.ContentUris
+import android.net.Uri
+
+object PlayerUtils {
+
+    fun getAlbumArtUri(albumId: Long) =
+        ContentUris.withAppendedId(Uri.parse("content://media/external/audio/albumart"), albumId)
+
+}
